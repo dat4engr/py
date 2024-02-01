d839fe49d2dbf986a60aedaf716eae0a4b455a27
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-02-01 23:48:43 +0800

    Update weather_app.py
    Adding concise docstrings.

--- pipelines/weather_app/weather_app.py
+++ pipelines/weather_app/weather_app.py
@@ -3,27 +3,27 @@ from pyowm import OWM
 from datetime import datetime
 import json
 
-def get_coordinates(location: str):
+def get_coordinates(location: str) -> Union[Tuple[float, float], None]:
+    # Get the latitude and longitude coordinates of a location.
     g = geocoder.osm(location)
     return g.latlng if g.latlng else None
 
-def get_current_weather(lat: float, lon: float, api_key: str):
+def get_current_weather(lat: float, lon: float, api_key: str) -> Tuple[str, str, Any]:
+    # Get the current weather information for a specified location.
     owm = OWM(api_key)
     mgr = owm.weather_manager()
 
     observation = mgr.weather_at_coords(lat, lon)
-    # Format the current date and time
     current_date = datetime.now().strftime('%m-%d-%Y')
     current_time = datetime.now().strftime('%H:%M:%S')
-    # Return the formatted current date and time along with the weather
     return current_date, current_time, observation.weather
 
-def fetch_weather_data(location: str, api_key: str):
+def fetch_weather_data(location: str, api_key: str) -> None:
+    # Fetch weather data for a specified location and save it to a JSON file.
     coordinates = get_coordinates(location)
 
     if coordinates:
         lat, lon = coordinates
-        # Get the formatted current date, time, and weather
         current_date, current_time, weather = get_current_weather(lat, lon, api_key)
 
         weather_data = {
@@ -35,16 +35,13 @@ def fetch_weather_data(location: str, api_key: str):
         }
 
         try:
-            # Load existing weather data from the JSON file
             with open('weather_data.json', 'r') as file:
                 existing_data = json.load(file)
         except FileNotFoundError:
             existing_data = []
 
-        # Append the new weather data to the existing data
         existing_data.append(weather_data)
 
-        # Save the updated data back to the JSON file
         with open('weather_data.json', 'w') as file:
             json.dump(existing_data, file, indent=4)
 
@@ -55,4 +52,4 @@ def fetch_weather_data(location: str, api_key: str):
     else:
         print("Unable to fetch weather data for the location.")
 
-fetch_weather_data("Magalang,PH", "Your_API_Key_Here")
+fetch_weather_data("Magalang,PH", "5c9026775828973746c850fa10e2f45c")
