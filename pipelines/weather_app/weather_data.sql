de1812313ed761f1ba36aced1f2d4e351b8968a2
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-02-04 00:55:57 +0800

    Update weather_data.sql
    Updating SQL sample file after changing the collected data's date format.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,6 +1,2 @@
 INSERT INTO public.weather_data ("location","date","time",temperature,weather_status) VALUES
-	 ('Magalang, PH','2024-02-03','17:07:34',32.62,'Clouds'),
-	 ('Magalang, PH','2024-02-03','17:07:34',32.62,'Clouds'),
-	 ('Magalang, PH','2024-02-03','17:07:34',32.62,'Clouds'),
-	 ('Magalang, PH','2024-02-03','17:07:34',32.62,'Clouds'),
-	 ('Magalang, PH','2024-02-03','17:28:24',32.62,'Clouds');
+	 ('Magalang, PH','2024-02-04','00:52:08',26.62,'Clouds');
