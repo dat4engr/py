2d9cfb489ce52068c21d5c2fa9ce138a8433ca46
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-02-05 21:16:11 +0800

    Update weather_app.py
    Code restructured. Added 2 data for fetching and storing.

--- pipelines/weather_app/weather_app.py
+++ pipelines/weather_app/weather_app.py
@@ -66,7 +66,12 @@ class WeatherDataFetcher:
             observation = weather_manager.weather_at_coords(lat, lon)
             current_date = datetime.now().strftime('%Y-%m-%d')
             current_time = datetime.now().strftime('%H:%M:%S')
-            return current_date, current_time, observation.weather
+            
+            weather = observation.weather
+            wind = weather.wind()
+            humidity = weather.humidity
+            
+            return current_date, current_time, weather, wind, humidity
         except Exception as current_weather_retrieval_error:
             logging.error(f"Error getting current weather: {current_weather_retrieval_error}")
             return "", "", None, ErrorCode.CURRENT_WEATHER_RETRIEVAL_ERROR
@@ -80,16 +85,18 @@ class WeatherDataFetcher:
                 coordinates = self.get_coordinates(location)
                 if coordinates[1]:
                     lat, lon = coordinates
-                    current_date, current_time, weather = self.get_current_weather(lat, lon)
+                    current_date, current_time, weather, wind, humidity = self.get_current_weather(lat, lon)
                     if weather:
                         temperature = f"{weather.temperature('celsius')['temp']}"
                         weather_status = weather.status
                         weather_data = {
-                            'location': location,
                             'date': current_date,
                             'time': current_time,
+                            'location': location,
+                            'weather_status': weather_status,
                             'temperature': temperature,
-                            'weather_status': weather_status
+                            'wind_speed': wind['speed'],
+                            'humidity': humidity,
                         }
                         self.weather_cache[location] = weather_data
                     else:
@@ -105,10 +112,12 @@ class WeatherDataFetcher:
             json_handler = JSONHandler()
             json_handler.update_data(weather_data)
 
+            print(f"As of: {weather_data['date']} | {weather_data['time']}")
             print(f"Current weather at {location}:")
-            print(f"As of: {weather_data['date']} / {weather_data['time']}")
-            print(f"Temperature: {weather_data['temperature']}°C")
             print(f"Weather Status: {weather_data['weather_status']}")
+            print(f"Temperature: {weather_data['temperature']}°C")
+            print(f"Wind Speed: {weather_data['wind_speed']} m/s")
+            print(f"Humidity: {weather_data['humidity']}%")
 
         except FileNotFoundError:
             logging.error("Error: The weather data file does not exist.", ErrorCode.FILE_NOT_FOUND_ERROR)
@@ -146,16 +155,18 @@ class DatabaseHandler:
             cursor = self.db_conn.cursor()
 
             insert_query = '''
-            INSERT INTO weather_data (location, date, time, temperature, weather_status) 
-            VALUES (%s, %s, %s, %s, %s);
+            INSERT INTO weather_data (date, time, location, weather_status, temperature, wind_speed, humidity) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s);
             '''
 
             cursor.execute(insert_query, (
-                data['location'],
                 data['date'],
                 data['time'],
+                data['location'],
+                data['weather_status'],
                 data['temperature'],
-                data['weather_status']
+                data['wind_speed'],
+                data['humidity']
             ))
             self.db_conn.commit()
             logging.info("Data inserted into the database successfully.")
