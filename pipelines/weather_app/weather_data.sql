e40c2a9986ca2728dc7b363a6d2298dc47cd7880
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-02-10 22:38:50 +0800

    Update weather_data.sql
    Adding data fetched using updated code.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,7 +1,10 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity) VALUES
-	 ('2024-02-10','12:09:33','Angeles, PH','Clouds',30.04,3.11,44.0),
-	 ('2024-02-10','12:09:33','Magalang, PH','Clouds',30.42,2.59,43.0),
-	 ('2024-02-10','12:09:34','Mabalacat City, PH','Clouds',30.01,2.86,44.0),
-	 ('2024-02-10','12:10:24','Mabalacat City, PH','Clouds',30.01,2.86,44.0),
-	 ('2024-02-10','12:10:25','Magalang, PH','Clouds',30.42,2.59,43.0),
-	 ('2024-02-10','12:10:25','Angeles, PH','Clouds',30.04,3.11,44.0);
+	 ('2024-02-10','22:15:49','Angeles, PH','Clouds',25.25,2.06,73.0),
+	 ('2024-02-10','22:15:50','Mabalacat City, PH','Clouds',25.22,2.06,73.0),
+	 ('2024-02-10','22:15:50','Magalang, PH','Clouds',25.62,2.06,73.0),
+	 ('2024-02-10','22:18:31','Magalang, PH','Clouds',25.62,2.06,73.0),
+	 ('2024-02-10','22:18:31','Angeles, PH','Clouds',25.25,2.06,73.0),
+	 ('2024-02-10','22:18:31','Mabalacat City, PH','Clouds',25.22,2.06,73.0),
+	 ('2024-02-10','22:19:19','Mabalacat City, PH','Clouds',25.22,2.06,73.0),
+	 ('2024-02-10','22:19:19','Magalang, PH','Clouds',25.62,2.06,73.0),
+	 ('2024-02-10','22:19:20','Angeles, PH','Clouds',25.25,2.06,73.0);
