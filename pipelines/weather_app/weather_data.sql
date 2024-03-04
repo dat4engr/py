006c14423e9ced62101c4566b10f530c74b218f9
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-03-04 22:09:11 +1000

    Update weather_data.sql
    Add data transform of text-based to lowercase for uniformity.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity) VALUES
-	 ('2024-03-04','11:32:11','Angeles, Ph','Clouds',28.25,2.06,61.0),
-	 ('2024-03-04','11:32:12','Mabalacat City, Ph','Clouds',28.22,2.06,61.0),
-	 ('2024-03-04','11:32:12','Magalang, Ph','Clouds',28.62,2.06,61.0);
+	 ('2024-03-04','22:05:03','mabalacat city, ph','Clouds',28.22,2.06,69.0),
+	 ('2024-03-04','22:05:04','magalang, ph','Clouds',28.62,2.06,69.0),
+	 ('2024-03-04','22:05:04','angeles, ph','Clouds',28.25,2.06,69.0);
