4384691dbc4bd6f8c6989a17153610e562a3ba95
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-03-05 14:56:57 +1000

    Update weather_data.sql
    Include weather_status for data transforming to lowercase and normalize latitude and longitude of locations’ list.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity) VALUES
-	 ('2024-03-04','22:05:03','mabalacat city, ph','Clouds',28.22,2.06,69.0),
-	 ('2024-03-04','22:05:04','magalang, ph','Clouds',28.62,2.06,69.0),
-	 ('2024-03-04','22:05:04','angeles, ph','Clouds',28.25,2.06,69.0);
+	 ('2024-03-05','14:49:19','angeles, ph','clouds',32.25,4.12,51.0),
+	 ('2024-03-05','14:49:19','mabalacat city, ph','clouds',32.22,4.12,51.0),
+	 ('2024-03-05','14:49:20','magalang, ph','clouds',32.62,4.12,51.0);
