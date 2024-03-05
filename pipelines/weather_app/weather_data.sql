945202e96cb2d03233aef84131d7856e0056ae35
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-03-06 06:39:30 +1000

    Update weather_data.sql
    Continue to data normalize weather values

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity) VALUES
-	 ('2024-03-05','14:49:19','angeles, ph','clouds',32.25,4.12,51.0),
-	 ('2024-03-05','14:49:19','mabalacat city, ph','clouds',32.22,4.12,51.0),
-	 ('2024-03-05','14:49:20','magalang, ph','clouds',32.62,4.12,51.0);
+	 ('2024-03-06','06:36:17','mabalacat city, ph','clouds',23.22,0.46,78.0),
+	 ('2024-03-06','06:36:18','magalang, ph','clouds',23.62,0.46,78.0),
+	 ('2024-03-06','06:36:18','angeles, ph','clouds',23.25,0.46,78.0);
