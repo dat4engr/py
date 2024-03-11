19d9ca04498ab356cf1383646670dee7a05352cf
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-03-11 12:26:39 +1000

    Update weather_data.sql
    Added updated data using the updated code to fetch weather data.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity) VALUES
-	 ('2024-03-10','15:17:54','magalang, ph','clouds',31.62,1.84,48.0),
-	 ('2024-03-10','15:17:54','mabalacat city, ph','clouds',31.22,1.84,48.0),
-	 ('2024-03-10','15:17:54','angeles, ph','clouds',31.25,1.84,48.0);
+	 ('2024-03-11','12:19:36','mabalacat city, ph','clouds',28.22,0.92,57.0),
+	 ('2024-03-11','12:19:36','magalang, ph','clouds',28.62,0.92,57.0),
+	 ('2024-03-11','12:19:37','angeles, ph','clouds',29.33,1.74,49.0);
