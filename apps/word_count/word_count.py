c17d05fc14b784e57634d46b76e855626b68206d
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-04-02 05:33:07 +1000

    Update word_count.py
    Optimize word_type function, simplify the function by using a single if-elif-else block. Improve user interaction as getting clearer instructions and feedback while interacting with the application.

--- apps/word_count/word_count.py
+++ apps/word_count/word_count.py
@@ -9,8 +9,8 @@ def load_spacy_model(model_name):
     except OSError as error:
         logging.error(f"Error loading Spacy model: {error}")
         return None
-    except Exception as e:
-        logging.error(f"An error occurred while loading Spacy model: {e}")
+    except Exception as exception:
+        logging.error(f"An error occurred while loading Spacy model: {exception}")
         return None
 
 def validate_word(text, nlp):
@@ -22,8 +22,8 @@ def validate_word(text, nlp):
         else:
             logging.error(f"Invalid input: {text}, contains non-alphabetic characters.")
             return False
-    except Exception as e:
-        logging.error(f"An error occurred while validating word: {e}")
+    except Exception as exception:
+        logging.error(f"An error occurred while validating word: {exception}")
         return False
 
 def get_user_input():
@@ -36,7 +36,7 @@ def get_user_input():
         exit()
 
     while text != 'q':
-        text = input("Enter some text (or 'q' to quit): ").lower()
+        text = input("Enter a word or sentence (or 'q' to quit): ").lower()
 
         if text == 'q':
             return text
@@ -45,7 +45,18 @@ def get_user_input():
             return text
         else:
             logging.error(f"Invalid input {text}. Please enter a valid English word or 'q' to quit.")
-            print("Invalid input. Please enter a valid English word or 'q' to quit.")
+            print("Invalid input. Please enter a valid English word or sentence or 'q' to quit.")
+
+def word_type(token):
+    # Determine the word type (Noun, Verb, Adjective) for a given token.
+    if token.pos_ == 'NOUN':
+        return 'Noun'
+    elif token.pos_ == 'VERB':
+        return 'Verb'
+    elif token.pos_ == 'ADJ':
+        return 'Adjective'
+    else:
+        return 'Other'
 
 def process_text(input_text, nlp):
     # Process the input text using the Spacy model and print relevant information.
@@ -53,9 +64,13 @@ def process_text(input_text, nlp):
         doc = nlp(input_text)
         word_count = len(doc)
 
-        nouns = [token.text for token in doc if token.pos_ == 'NOUN']
-        verbs = [token.text for token in doc if token.pos_ == 'VERB']
-        adjectives = [token.text for token in doc if token.pos_ == 'ADJ']
+        word_types = {}
+        for token in doc:
+          wt = word_type(token)
+          if wt in word_types:
+              word_types[wt].append(token.text)
+          else:
+              word_types[wt] = [token.text]
 
         sentence_structure = {}
         for sent in doc.sents:
@@ -70,21 +85,20 @@ def process_text(input_text, nlp):
             readability_suggestions.append("Consider breaking down the text into shorter sentences.")
 
         print(f"Number of words: {word_count}")
-        print(f"Nouns: {nouns}")
-        print(f"Verbs: {verbs}")
-        print(f"Adjectives: {adjectives}")
+        for wt, words in word_types.items():
+            print(f"{wt}s: {words}")
         print(f"Sentence Structure: {sentence_structure}")
         print("Readability Suggestions:")
         for suggestion in readability_suggestions:
             print(suggestion)
-    except Exception as e:
-        logging.error(f"An error occurred while processing text: {e}")
+    except Exception as exception:
+        logging.error(f"An error occurred while processing text: {exception}")
 
 def main():
     # The main function to run the Word Count App and handle user input.
     try:
         logging.basicConfig(filename='error.log', level=logging.ERROR)
-        print("Welcome to Word Count App!")
+        print("Welcome to the Word Count App!")
         nlp = load_spacy_model("en_core_web_sm")
 
         if nlp is None:
@@ -99,10 +113,10 @@ def main():
 
             logging.info(f"Processing text: {text}")
             process_text(text, nlp)
-
             print()
-    except Exception as e:
-        logging.error(f"An error occurred in the main function: {e}")
+
+    except Exception as exception:
+        logging.error(f"An error occurred in the main function: {exception}")
 
 if __name__ == "__main__":
     main()
