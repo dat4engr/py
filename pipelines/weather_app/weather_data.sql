387ef1efb1f62dd3229f86fa8f1546cc4a302256
Carlo <113097127+dat4engr@users.noreply.github.com>
2024-04-24 11:51:54 +1000

    Update weather_data.sql
    Added data using the updated code to fetch weather data.

--- pipelines/weather_app/weather_data.sql
+++ pipelines/weather_app/weather_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.weather_data ("date","time","location",weather_status,temperature,wind_speed,humidity,climate_data) VALUES
-	 ('2024-04-01','18:40:33','angeles, ph','clouds',35.25,1.38,38.0,'{"date": "2024-04-01", "time": "18:40:33", "humidity": 38, "location": "angeles, ph", "wind_speed": 1.38, "temperature": 35.25, "weather_status": "clouds"}'),
-	 ('2024-04-01','18:40:35','mabalacat city, ph','clouds',35.22,1.38,38.0,'{"date": "2024-04-01", "time": "18:40:35", "humidity": 38, "location": "mabalacat city, ph", "wind_speed": 1.38, "temperature": 35.22, "weather_status": "clouds"}'),
-	 ('2024-04-01','18:40:37','magalang, ph','clouds',35.62,1.38,38.0,'{"date": "2024-04-01", "time": "18:40:37", "humidity": 38, "location": "magalang, ph", "wind_speed": 1.38, "temperature": 35.62, "weather_status": "clouds"}');
+	 ('2024-04-24','11:48:00','angeles, ph','clear',34.01,1.39,47.0,'{"date": "2024-04-24", "time": "11:48:00", "humidity": 47, "location": "angeles, ph", "wind_speed": 1.39, "temperature": 34.01, "weather_status": "clear"}'),
+	 ('2024-04-24','11:48:03','mabalacat city, ph','clear',33.89,1.35,48.0,'{"date": "2024-04-24", "time": "11:48:03", "humidity": 48, "location": "mabalacat city, ph", "wind_speed": 1.35, "temperature": 33.89, "weather_status": "clear"}'),
+	 ('2024-04-24','11:48:05','magalang, ph','clear',34.32,1.13,48.0,'{"date": "2024-04-24", "time": "11:48:05", "humidity": 48, "location": "magalang, ph", "wind_speed": 1.13, "temperature": 34.32, "weather_status": "clear"}');
