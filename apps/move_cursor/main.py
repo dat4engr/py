d4749bfc9315ccb5d8eb1a2d710267c214055413
Carlo <113097127+carlolax@users.noreply.github.com>
2024-06-16 08:59:34 +1000

    Update main.py
    Improve the main.py’s error handling on possible errors.

--- apps/move_cursor/main.py
+++ apps/move_cursor/main.py
@@ -3,9 +3,12 @@ import user_input
 
 def main():
     # Main function
-    num_moves = user_input.get_user_input()
-    cursor_movement.move_cursor_like_person(num_moves)
-    print("Cursor movements completed!")
+    try:
+        num_moves = user_input.get_user_input()
+        cursor_movement.move_cursor_like_person(num_moves)
+        print("Cursor movements completed!")
+    except Exception as exception:
+        print(f"An error occurred: {exception}")
 
 if __name__ == "__main__":
     main()
