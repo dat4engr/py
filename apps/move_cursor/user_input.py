8f19a81113478b728ffc31fcfc022e673835e3ea
Carlo <113097127+carlolax@users.noreply.github.com>
2024-06-17 16:13:28 +1000

    Update user_input.py
    Add type annotations to understand what types of data are expected and returned from each function.

--- apps/move_cursor/user_input.py
+++ apps/move_cursor/user_input.py
@@ -1,8 +1,8 @@
-def get_user_input():
+def get_user_input() -> int:
     # Function to get user input for the number of cursor moves
     while True:
         try:
-            num_moves = int(input("Enter the number of cursor moves you want to make (positive integer that is greater than 0): "))
+            num_moves: int = int(input("Enter the number of cursor moves you want to make (positive integer that is greater than 0): "))
             if num_moves <= 0:
                 print("Please enter a positive integer greater than 0.")
                 continue
